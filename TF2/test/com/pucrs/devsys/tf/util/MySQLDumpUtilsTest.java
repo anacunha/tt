93f4d1e5989549e5d26e9899d9264ae6197b0366
ihberbigier <israel.berbigier@gmail.com>
2013-12-03 17:54:44 -0200

    teste unitario do backup e restore

--- TF2/test/com/pucrs/devsys/tf/util/MySQLDumpUtilsTest.java
+++ TF2/test/com/pucrs/devsys/tf/util/MySQLDumpUtilsTest.java
@@ -2,17 +2,59 @@ package com.pucrs.devsys.tf.util;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class MySQLDumpUtilsTest {
 
+
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getDumpBackupOk() {
+		MySQLDumpUtils md = new MySQLDumpUtils();
+				try {
+					md.backup();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
 	}
 	
-	public void getDumpOk() {
-		
+	@Test
+	public void getDumpBackupNotOk() {
+		MySQLDumpUtils md = new MySQLDumpUtils();
+				try {
+					md.dump( "C:/Users/israelberbigier/Documents/m", "", "88hbtb", "basedetest" );  
+				} catch (Exception e) {
+					
+					assertEquals(new IOException(), e);
+				}
+				
 	}
+	
+	
+	@Test
+	public void getDumpRestoreOk() {
+		MySQLDumpUtils md = new MySQLDumpUtils();
+				try {
+					md.restore();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+	}
+	
+	@Test
+	public void getDumpRestoreNotOk() {
+		MySQLDumpUtils md = new MySQLDumpUtils();
+		try {
+			md.restore("C:\\Users\\israelberbigier\\Documents\\minhaBase.sql", "localho", "3006", "root", "88hbtb", "basedetest");
+		} catch (Exception e) {
+			assertEquals(new IOException(), e);
+		}  
+	}
+	
+	
 
 }
