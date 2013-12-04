b0c2fd96c99283e8e81ba7cab9cb22f0575b5615
ihberbigier <israel.berbigier@gmail.com>
2013-12-03 22:25:48 -0200

    atualização da atualização de evento

--- TF2/src/com/pucrs/devsys/tf/rest/EventService.java
+++ TF2/src/com/pucrs/devsys/tf/rest/EventService.java
@@ -1,6 +1,9 @@
 package com.pucrs.devsys.tf.rest;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import com.pucrs.devsys.tf.db.AbstractCRUD;
 import com.pucrs.devsys.tf.persistence.Event;
@@ -8,4 +11,14 @@ import com.pucrs.devsys.tf.persistence.Event;
 @Path("events")
 public class EventService extends AbstractCRUD<Event> {
 
+	@Override
+	@PUT
+	@Consumes("application/json")
+	@Produces("application/json")
+	@Path("/{id}")
+	public Boolean update(Event instance)
+	{
+		// TODO Auto-generated method stub
+		return super.update(instance);
+	}
 }
